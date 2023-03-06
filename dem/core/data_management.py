f14b53bad63237e76be23758b3b09b84ae94d1aa
janosmurai <murai.janos@gmail.com>
2023-03-06 14:36:07 +0100

    File parsing implemented.

--- dem/core/data_management.py
+++ dem/core/data_management.py
@@ -1,16 +1,20 @@
 """dev_env.json handling."""
 # dem/core/data_management.py
 
-from pathlib import Path
+from pathlib import PurePath
 import os
 import json
 
 def get_deserialized_dev_env_json():
-	#Get the raw json file.
-	dev_env_json_path = Path(os.path.expanduser('~') + "/.config/axem/dev_env.json")
-	dev_env_json = open(dev_env_json_path, "r")
+    #Get the raw json file.
+    dev_env_json_path = PurePath(os.path.expanduser('~') + "/.config/axem/dev_env.json")
+    dev_env_json = open(dev_env_json_path, "r")
 
-	#Parse the json file.
-	dev_env_json_deserialized = json.load(dev_env_json)
+    return json.load(dev_env_json)
 
-	return dev_env_json_deserialized
+def get_deserialized_dev_env_org_json():
+    #Get the raw json file.
+    dev_env_org_json_path = PurePath("example_json/dev_env_org.json")
+    dev_env_org_json = open(dev_env_org_json_path, "r")
+
+    return json.load(dev_env_org_json)
