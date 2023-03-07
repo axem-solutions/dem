dbfe985ea9aab803a79e26da1b85ed5dc8051bb6
janosmurai <murai.janos@gmail.com>
2023-03-07 17:31:46 +0100

    Refactor.

--- dem/cli/command/list_command.py
+++ dem/cli/command/list_command.py
@@ -5,6 +5,32 @@ from dem.core import container_engine, data_management, dev_env_setup, registry
 from dem.cli.console import stdout, stderr
 from rich.table import Table
 
+(
+    DEV_ENV_ORG_NOT_IN_REGISTRY,
+    DEV_ENV_ORG_INSTALLED_LOCALLY,
+    DEV_ENV_ORG_REAINSTALL,
+    DEV_ENV_ORG_READY,
+) = range(4)
+
+(
+    DEV_ENV_LOCAL_NOT_AVAILABLE,
+    DEV_ENV_LOCAL_REINSTALL,
+    DEV_ENV_LOCAL_INSTALLED,
+) = range(3)
+
+dev_env_org_status_messages = {
+    DEV_ENV_ORG_NOT_IN_REGISTRY: "[red]Error: Required image is not available in the registry![/]",
+    DEV_ENV_ORG_INSTALLED_LOCALLY: "Installed locally.",
+    DEV_ENV_ORG_REAINSTALL: "Incopmlete local install. Reinstall needed.",
+    DEV_ENV_ORG_READY: "Ready to install.",
+}
+
+dev_env_local_status_messages = {
+    DEV_ENV_LOCAL_NOT_AVAILABLE: "[red]Error: Required image is not available![/]",
+    DEV_ENV_LOCAL_REINSTALL: "Incopmlete local install. Reinstall needed.",
+    DEV_ENV_LOCAL_INSTALLED: "Installed.",
+}
+
 def is_dev_env_org_installed_locally(dev_env_org: dev_env_setup.DevEnvOrg) -> bool:
     dev_env_json_deserialized = data_management.get_deserialized_dev_env_json()
     dev_env_local_setup_obj = dev_env_setup.DevEnvLocalSetup(dev_env_json_deserialized)
@@ -16,22 +42,25 @@ def get_dev_env_status(dev_env: dev_env_setup.DevEnvLocal | dev_env_setup.DevEnv
     dev_env_status = ""
     if isinstance(dev_env, dev_env_setup.DevEnvOrg):
         if (dev_env_setup.IMAGE_NOT_AVAILABLE in image_statuses) or (dev_env_setup.IMAGE_LOCAL_ONLY in image_statuses):
-            dev_env_status = "[red]Error: Required image is not available in the registry![/]"
+            dev_env_status = dev_env_org_status_messages[DEV_ENV_ORG_NOT_IN_REGISTRY]
         elif (image_statuses.count(dev_env_setup.IMAGE_LOCAL_AND_REGISTRY) == len(image_statuses)) and \
                 (is_dev_env_org_installed_locally(dev_env) == True):
-            dev_env_status = "Installed locally."
+            dev_env_status = dev_env_org_status_messages[DEV_ENV_ORG_INSTALLED_LOCALLY]
         else:
             if (is_dev_env_org_installed_locally(dev_env) == True):
-                dev_env_status = "Incopmlete local install. Reinstall needed."
+                dev_env_status = dev_env_org_status_messages[DEV_ENV_ORG_REAINSTALL]
             else:
-                dev_env_status = "Ready to install."
+                dev_env_status = dev_env_org_status_messages[DEV_ENV_ORG_READY]
     else:
         if (dev_env_setup.IMAGE_NOT_AVAILABLE in image_statuses):
             dev_env_status = "[red]Error: Required image is not available![/]"
+            dev_env_status = dev_env_local_status_messages[DEV_ENV_LOCAL_NOT_AVAILABLE]
         elif (dev_env_setup.IMAGE_REGISTRY_ONLY in image_statuses):
             dev_env_status = "Incopmlete local install. Reinstall needed."
+            dev_env_status = dev_env_local_status_messages[DEV_ENV_LOCAL_REINSTALL]
         else:
             dev_env_status = "Installed."
+            dev_env_status = dev_env_local_status_messages[DEV_ENV_LOCAL_INSTALLED]
     return dev_env_status
 
 def execute(local: bool, all: bool, env: bool) -> None:
