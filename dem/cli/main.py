fc1e888f57fe59298e2e333579794bc98c65fed2
janosmurai <murai.janos@gmail.com>
2023-03-21 18:21:25 +0100

    Help info added for the available commands.

--- dem/cli/main.py
+++ dem/cli/main.py
@@ -12,15 +12,32 @@ typer_cli = typer.Typer()
 def list(local: bool = typer.Option(False, help="Scope is the local host."),
          all: bool = typer.Option(False, help="Scope is the organization."),
          env: bool = typer.Option(False, help="List the environments.")) -> None:
+    """
+    List the available Development Environments available locally or for the organization.
+    
+    The following option combinations suppported:
+
+        --local --env -> List the local Development Environments.
+
+        --all --env -> List the organization's Development Environments.
+    """
     list_cmd.execute(local, all, env)
 
 @typer_cli.command()
-def info(dev_env_name: str) -> None:
+def info(dev_env_name: str = typer.Argument(...,
+                                            help="Name of the Development Environment to get info about.")) -> None:
+    """
+    Get information about the specified Development Environment.
+    """
     info_cmd.execute(dev_env_name)
 
 @typer_cli.command()
 def pull(dev_env_name: str = typer.Argument(..., 
                                             help="Name of the Development Environment to install.")) -> None:
+    """
+    Pull all the required containerized tools from the registry and install the Development 
+    Environment locally.
+    """
     pull_cmd.execute(dev_env_name)
 
 def _version_callback(value: bool) -> None:
@@ -37,6 +54,10 @@ def main(
         help="Show the dem version.",
         callback=_version_callback,
         is_eager=True,
-    )
-) -> None:
+    )) -> None:
+    """
+    Development Environment Manager (dem)
+    
+    Manage your containerized development environments with ease.
+    """
     return
