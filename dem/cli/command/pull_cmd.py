3f0d959aa80d9e0682b1acb727c4913bc01b1e04
janosmurai <murai.janos@gmail.com>
2023-03-25 14:49:10 +0100

    Test case added for installed, but different Dev Env.

--- dem/cli/command/pull_cmd.py
+++ dem/cli/command/pull_cmd.py
@@ -7,6 +7,40 @@ from dem.core import dev_env_setup as dev_env_setup, \
                      registry as registry
 from dem.cli.console import stdout, stderr
 
+def install_to_dev_env_json(dev_env_local: dev_env_setup.DevEnvLocal, 
+                            dev_env_org: dev_env_setup.DevEnvOrg,
+                            dev_env_local_setup: dev_env_setup.DevEnvLocalSetup) -> dev_env_setup.DevEnvLocal:
+    """Install the Dev Env descriptor to the dev_env.json.
+    
+    If the dev_env_local is None, the Dev Env is not yet installed locally. Install it by appending 
+    the Dev Env descriptor to the dev_env.json file.
+    If the Dev Env is already installed, but the descriptor is different, then overwrite the 
+    dev_env.json element with the new one.
+    If the Dev Env is already installed and both the descriptors are the same, then there is nothing
+    to do. 
+    Args:
+    dev_env_local -- local Dev Env instance (none if not yet installed)
+    dev_env_org -- organization's Dev Env instance
+    dev_env_local_setup -- the local Dev Env setup
+    Return with the dev_env_local instance.
+    """
+    if dev_env_local is None:
+        # If not available, install it.
+        dev_env_local = dev_env_setup.DevEnvLocal(dev_env_org=dev_env_org)
+        dev_env_local_setup.dev_envs.append(dev_env_local)
+        deserialized_local_dev_env = dev_env_local_setup.get_deserialized()
+        data_management.write_deserialized_dev_env_json(deserialized_local_dev_env)
+    elif dev_env_local.tools != dev_env_org.tools:
+        # If already installed, but different, then overwrite it.
+        dev_env_local_setup.dev_envs.remove(dev_env_local)
+        dev_env_local.tools = dev_env_org.tools
+        dev_env_local_setup.dev_envs.append(dev_env_local)
+        deserialized_local_dev_env = dev_env_local_setup.get_deserialized()
+        data_management.write_deserialized_dev_env_json(deserialized_local_dev_env)
+
+    return dev_env_local
+
+
 def execute(dev_env_name: str) -> None:
     #Get the organization's Dev Env if available.
     dev_env_org_json_deserialized = data_management.read_deserialized_dev_env_org_json()
@@ -20,24 +54,7 @@ def execute(dev_env_name: str) -> None:
     dev_env_local_setup = dev_env_setup.DevEnvLocalSetup(dev_env_local_json_deserialized)
     dev_env_local = dev_env_org.get_local_instance(dev_env_local_setup)
 
-    #Check that the Dev Env is already installed in the dev_env.json file.
-    if dev_env_local is not None:
-        # Look for any difference between the local and the org Dev Env.
-        if dev_env_local.tools != dev_env_org.tools:
-            dev_env_local_setup.dev_envs.remove(dev_env_local)
-            dev_env_local.tools = dev_env_org.tools
-            dev_env_local_setup.dev_envs.append(dev_env_local)
-            deserialized_local_dev_env = dev_env_local_setup.get_deserialized()
-            data_management.write_deserialized_dev_env_json(deserialized_local_dev_env)
-        else:
-            # The local and in org instance are the same.
-            pass
-    else:
-        # If not available, install it.
-        dev_env_local = dev_env_setup.DevEnvLocal(dev_env_org=dev_env_org)
-        dev_env_local_setup.dev_envs.append(dev_env_local)
-        deserialized_local_dev_env = dev_env_local_setup.get_deserialized()
-        data_management.write_deserialized_dev_env_json(deserialized_local_dev_env)
+    dev_env_local = install_to_dev_env_json(dev_env_local, dev_env_org, dev_env_local_setup)
 
     #The local DevEnvSetup contains the DevEnvOrg to install. Check the images' status
     container_engine_obj = container_engine.ContainerEngine()
@@ -52,8 +69,6 @@ def execute(dev_env_name: str) -> None:
             stdout.print("Pulling image: " + image_to_pull)
             container_engine_obj.pull(image_to_pull)
 
-
-    #Tmp validation:
     local_images = container_engine_obj.get_local_image_tags()
     image_statuses = dev_env_local.check_image_availability(local_images, registry_images)
 
