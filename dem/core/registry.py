9364b252e71dc698821978faf03aa81ba80b2a33
janosmurai <murai.janos@gmail.com>
2023-04-19 17:06:09 +0200

    Get the image tags from the Docker Hub Registry using its API.

--- dem/core/registry.py
+++ dem/core/registry.py
@@ -3,26 +3,21 @@ Use the container engine when possible for accessing the registry.
 """
 # dem/core/registry.py
 
-import json
-import docker
-import subprocess
-
+import requests
 from dem.core import container_engine as container_engine
 
 def list_repos(container_engine_obj: container_engine.ContainerEngine) -> list[str]:
-    registryimagelist = []
-    images = []
-
+    registry = "axemsolutions"
     container_engine_obj = container_engine.ContainerEngine()
+    images = []
+    
+    for image in container_engine_obj.search(registry):
+        url = f"https://registry.hub.docker.com/v2/repositories/{image}/tags/"
 
-    registryimagelist = container_engine_obj.search("axemsolutions")
-                
-    for image in registryimagelist:        
-        cmd = "docker trust inspect " + image        
-        result = subprocess.run(cmd, shell=True,stdout=subprocess.PIPE)
-        resultjson=json.loads(result.stdout.decode('UTF-8'))
-        signedTags=resultjson[0]['SignedTags']
-        for tag in signedTags:            
-            images.append(image + ":" + tag['SignedTag'])
-        
+        response = requests.get(url)
+
+        if response.status_code == requests.codes.ok:
+            for result in response.json()["results"]:
+                images.append(image + ":" + result["name"])
+
     return images
