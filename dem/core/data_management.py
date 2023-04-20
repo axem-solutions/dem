c89f6e0a2695bf6b577b91b75ee5ee4467ff228f
janosmurai <murai.janos@gmail.com>
2023-04-20 19:57:31 +0200

    Empty dev_env.json gets created if it doesn't exist. Tests added.

--- dem/core/data_management.py
+++ dem/core/data_management.py
@@ -7,18 +7,51 @@ import json, requests
 
 # Local dev_env.json
 
+_empty_dev_env_json = """
+{
+    "version": "0.1",
+    "org_name": "axem",
+    "registry": "registry-1.docker.io",
+    "development_environments": []
+}
+"""
+
+class DevEnvJSON():
+    _path = PurePath(os.path.expanduser('~') + "/.config/axem/dev_env.json")
+
+    def _create_empty_dev_env_json(self) -> None:
+        dev_env_json = open(self._path, "w")
+        dev_env_json.write(_empty_dev_env_json)
+        dev_env_json.close()
+
+    def __init__(self) -> None:
+        self.deserialized = ""
+
+    def read(self) -> dict:
+        try:
+            dev_env_json = open(self._path, "r")
+        except FileNotFoundError:
+            self._create_empty_dev_env_json()
+            self.deserialized = json.loads(_empty_dev_env_json)
+        else:
+            self.deserialized = json.load(dev_env_json)
+            dev_env_json.close()
+        
+        return self.deserialized
+
+    def write(self, deserialized: dict) -> None:
+        self.deserialized = deserialized
+        dev_env_json = open(self._path, "w")
+        json.dump(deserialized, dev_env_json, indent=4)
+        dev_env_json.close()
+
 def read_deserialized_dev_env_json():
-    dev_env_json_path = PurePath(os.path.expanduser('~') + "/.config/axem/dev_env.json")
-    dev_env_json = open(dev_env_json_path, "r")
-    deserialized_dev_env_json = json.load(dev_env_json)
-    dev_env_json.close()
-    return deserialized_dev_env_json
+    dev_env_json = DevEnvJSON()
+    return dev_env_json.read()
 
 def write_deserialized_dev_env_json(dev_env_json_deserialized: dict) -> None:
-    dev_env_json_path = PurePath(os.path.expanduser('~') + "/.config/axem/dev_env.json")
-    dev_env_json = open(dev_env_json_path, "w")
-    json.dump(dev_env_json_deserialized, dev_env_json, indent=4)
-    dev_env_json.close()
+    dev_env_json = DevEnvJSON()
+    dev_env_json.write(dev_env_json_deserialized)
 
 # Organization specific dev_env_org.json
 
