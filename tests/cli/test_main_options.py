2c4b9ae561e1744fd04faf7ff01d2af8597cf64e
janosmurai <murai.janos@gmail.com>
2023-05-31 15:44:14 +0200

    Test case added for getting the version when DEM is installed.

--- tests/cli/test_main_options.py
+++ tests/cli/test_main_options.py
@@ -19,19 +19,27 @@ runner = CliRunner(mix_stderr=False)
 @patch("dem.cli.main.__app_name__", "axem-dem")
 @patch("dem.cli.main.stdout.print")
 @patch("dem.cli.main.importlib.metadata.version")
-def test_version_as_module(mock_importlib_metadata_version, mock_stdout_print):
+def test_version_as_installed(mock_importlib_metadata_version, mock_stdout_print):
     # Test setup
     test_version = "0.1.2"
     mock_importlib_metadata_version.return_value = test_version
 
     # Run unit under test
-    with pytest.raises(typer.Exit):
-        result = runner.invoke(main.typer_cli, ["--version"])
+    result = runner.invoke(main.typer_cli, ["--version"])
 
-        # Check expectations
-        assert result.exit_code == 0
+    # Check expectations
+    assert result.exit_code == 0
 
-        mock_stdout_print.assert_called_once_with("[cyan]" + main.__app_name__ + " v" + test_version)
+    mock_stdout_print.assert_called_once_with("[cyan]" + main.__app_name__ + " v" + test_version + "[/]")
 
-def test_version_as_installed():
-    pass
+@patch("dem.cli.main.stdout.print")
+@patch("dem.cli.main.importlib.metadata.version")
+def test_version_as_module(mock_importlib_metadata_version, mock_stdout_print):
+    # Test setup
+    mock_importlib_metadata_version.side_effect = Exception()
+
+    # Run unit under test
+    result = runner.invoke(main.typer_cli, ["-v"])
+
+    # Check expectations
+    assert result.exit_code == 0
