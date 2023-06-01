67bc3928d4c539bfff85eb68f7e7a358fffe012b
janosmurai <murai.janos@gmail.com>
2023-06-01 10:59:08 +0200

    Better output for user.

--- dem/cli/command/delete_cmd.py
+++ dem/cli/command/delete_cmd.py
@@ -6,16 +6,17 @@ from dem.cli.console import stderr, stdout
 import typer
 import docker.errors
 
-def try_to_delete_tool_image(tool_image: str, dev_env_local_setup: DevEnvLocalSetup):
+def try_to_delete_tool_image(tool_image: str, dev_env_local_setup: DevEnvLocalSetup) -> None:
     stdout.print("\nThe tool image [bold]" + tool_image + "[/bold] is not required by any Development Environment anymore.")
     if typer.confirm("Would you like to remove it?"):
-        pass
-        # try:
-        #     dev_env_local_setup.container_engine.remove(tool_image)
-        # except docker.errors.ImageNotFound:
-        #     stdout.print("[yellow]Couldn't delete " + tool_image + ", because doesn't exist.")
-        # except docker.errors.APIError:
-        #     stderr.print("[red]Error: " + tool_image + " is used by a container. Unable to remove it.")
+        try:
+            dev_env_local_setup.container_engine.remove(tool_image)
+        except docker.errors.ImageNotFound:
+            stdout.print("[yellow]Couldn't delete " + tool_image + ", because doesn't exist.\n")
+        except docker.errors.APIError:
+            stderr.print("[red]Error: " + tool_image + " is used by a container. Unable to remove it.\n")
+        else:
+            stdout.print("[green]Successfully removed![/]\n")
 
 def remove_unused_tool_images(deleted_dev_env: DevEnvLocal, dev_env_local_setup: DevEnvLocalSetup) -> None:
     all_required_tool_images = set()
@@ -36,9 +37,11 @@ def execute(dev_env_name: str) -> None:
     dev_env_to_delete = dev_env_local_setup.get_dev_env_by_name(dev_env_name)
 
     if dev_env_to_delete is None:
-        stderr.print("[red]The Development Environment doesn't exist.")
+        stderr.print("[red]Error: The [bold]" + dev_env_name + "[/bold] Development Environment doesn't exist.")
     else:
+        stdout.print("Deleting the Development Environment...")
         dev_env_local_setup.dev_envs.remove(dev_env_to_delete)
         dev_env_local_setup.update_json()
 
         remove_unused_tool_images(dev_env_to_delete, dev_env_local_setup)
+        stdout.print("[green]Successfully deleted the " + dev_env_name + "![/]")
