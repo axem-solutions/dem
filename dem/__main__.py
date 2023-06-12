a5dd3208aef0529d39b54b831d104e4e7b48870d
janosmurai <murai.janos@gmail.com>
2023-06-12 20:07:46 +0200

    Unused imports removed. Alignment should be set on the panel. Comments added. Refactor. New test cases added.

--- dem/__main__.py
+++ dem/__main__.py
@@ -4,42 +4,18 @@
 from dem import __command__
 from dem.cli.console import stderr, stdout
 from dem.core.exceptions import RegistryError
-<<<<<<< HEAD
-<<<<<<< HEAD
 from dem.core.dev_env_setup import DevEnvLocalSetup, DevEnvSetup
-=======
-from dem.core.dev_env_setup import DevEnvLocalSetup
->>>>>>> 5353bc3 (Started to work on the core callback mechanism.)
-=======
-from dem.core.dev_env_setup import DevEnvLocalSetup, DevEnvSetup
->>>>>>> 987018c (Callback mechanism added for the progress bar. Further tweaks in the output design.)
 import dem.cli.main, dem.cli.core_cb
 import docker.errors
+import types
 
 def main():
     """ Entry point for the CLI application"""
-<<<<<<< HEAD
-<<<<<<< HEAD
 
     # Set callback for core modules.
     DevEnvLocalSetup.invalid_json_cb = dem.cli.core_cb.user_confirm_cb
     DevEnvLocalSetup.msg_cb = dem.cli.core_cb.msg_cb
-<<<<<<< HEAD
-<<<<<<< HEAD
     DevEnvLocalSetup.pull_progress_cb = dem.cli.core_cb.pull_progress_cb
-=======
-=======
-
-    # Set callback for core modules.
->>>>>>> 763e6e6 (Minor fixes. Test cases added.)
-    DevEnvLocalSetup.core_cb = dem.cli.core_cb.core_cb
->>>>>>> 5353bc3 (Started to work on the core callback mechanism.)
-=======
-    DevEnvSetup.pull_progress_cb = dem.cli.core_cb.pull_progress_cb
->>>>>>> 987018c (Callback mechanism added for the progress bar. Further tweaks in the output design.)
-=======
-    DevEnvLocalSetup.pull_progress_cb = dem.cli.core_cb.pull_progress_cb
->>>>>>> 12a36b0 (Test cases fixed.)
 
     try:
         dem.cli.main.typer_cli(prog_name=__command__)
