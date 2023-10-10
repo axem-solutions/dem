15e8d6143861a0c8827e3edea9651368424892b1
Your Name <rabroldan06@gmail.com>
2023-10-10 12:40:57 -0400

    updated clone to cp

--- dem/cli/command/cp_cmd.py
+++ dem/cli/command/cp_cmd.py
@@ -1,4 +1,4 @@
-"""clone CLI command implementation."""
+"""cp CLI command implementation."""
 # dem/cli/command/rename_cmd.py
 
 import copy
@@ -6,14 +6,14 @@ from dem.core.dev_env import DevEnv
 from dem.core.platform import DevEnvLocalSetup
 from dem.cli.console import stderr
 
-def get_dev_env_to_clone(platform: DevEnvLocalSetup, dev_env_name: str) -> (DevEnv | None):
-    dev_env_to_clone = platform.get_dev_env_by_name(dev_env_name) 
+def get_dev_env_to_cp(platform: DevEnvLocalSetup, dev_env_name: str) -> (DevEnv | None):
+    dev_env_to_cp = platform.get_dev_env_by_name(dev_env_name) 
 
-    if dev_env_to_clone is None:
+    if dev_env_to_cp is None:
         stderr.print("[red]Error: The input Development Environment does not exist.[/]")
         return None
     else:
-        return dev_env_to_clone
+        return dev_env_to_cp
 
 def check_new_dev_env_name_taken(dev_env_local_setup: DevEnvLocalSetup, new_dev_env_name: str) -> bool:
     if dev_env_local_setup.get_dev_env_by_name(new_dev_env_name):
@@ -22,17 +22,17 @@ def check_new_dev_env_name_taken(dev_env_local_setup: DevEnvLocalSetup, new_dev_
     else:
         return False
 
-def clone_given_dev_env(platform: DevEnvLocalSetup, dev_env_to_clone: DevEnv,
+def cp_given_dev_env(platform: DevEnvLocalSetup, dev_env_to_cp: DevEnv,
                         new_dev_env_name: str):
-    new_dev_env = copy.deepcopy(dev_env_to_clone)
+    new_dev_env = copy.deepcopy(dev_env_to_cp)
     new_dev_env.name = new_dev_env_name
     platform.local_dev_envs.append(new_dev_env)
     platform.flush_to_file()
 
-def execute(dev_env_to_clone_name: str, new_dev_env_name: str) -> None:
+def execute(dev_env_to_cp_name: str, new_dev_env_name: str) -> None:
     platform = DevEnvLocalSetup()
-    dev_env_to_clone = get_dev_env_to_clone(platform, dev_env_to_clone_name)
+    dev_env_to_cp = get_dev_env_to_cp(platform, dev_env_to_cp_name)
 
-    if (dev_env_to_clone is not None and
+    if (dev_env_to_cp is not None and
         check_new_dev_env_name_taken(platform,new_dev_env_name) is False):
-        clone_given_dev_env(platform, dev_env_to_clone, new_dev_env_name)
+        cp_given_dev_env(platform, dev_env_to_cp, new_dev_env_name)
